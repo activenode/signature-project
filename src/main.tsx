a7fe500382e6813228fe28b88b2d03e3de023d7e
David Lorenz <info@activenode.de>
2024-12-20 21:09:44 +0100

    script update

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,6 @@ import './index.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <script defer src="https://cloud.umami.is/script.js" data-website-id="5d7adf9e-3b2b-4a8a-9435-2c35686b4c69"></script>
+
   </StrictMode>
 );
