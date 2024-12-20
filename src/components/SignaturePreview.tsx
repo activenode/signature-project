dfbd3b2b037eb631452e75c989309f3f28fd373f
David Lorenz <info@activenode.de>
2024-12-20 15:52:04 +0100

    fixed the font

--- src/components/SignaturePreview.tsx
+++ src/components/SignaturePreview.tsx
@@ -1,5 +1,5 @@
-import React, { forwardRef } from 'react';
-import { objectHasData } from '../utils/hasData';
+import React, { forwardRef } from "react";
+import { objectHasData } from "../utils/hasData";
 
 interface SignatureData {
   primaryLine: string;
@@ -9,83 +9,141 @@ interface SignatureData {
   email: string;
   logo: string;
   logoSize: number;
-
 }
 
-const SignaturePreview = forwardRef<HTMLDivElement, { data: SignatureData }>(({ data }, ref) => {
-  // Calculate the actual width in pixels based on the max-width and percentage
-  const logoWidth = (120 * data.logoSize) / 100;
+const SignaturePreview = forwardRef<HTMLDivElement, { data: SignatureData }>(
+  ({ data }, ref) => {
+    // Calculate the actual width in pixels based on the max-width and percentage
+    const logoWidth = (120 * data.logoSize) / 100;
 
-  const baseTextColor = '#6b7280';
-  const emailTextColor = '#2563eb';
-  const hasTextData = objectHasData(data, ['logoSize', 'logo']);
+    const baseTextColor = "#6b7280";
+    const primaryLineColor = "black";
+    const emailTextColor = "#2563eb";
+    const hasTextData = objectHasData(data, ["logoSize", "logo"]);
+    const baseTextSize = "17px";
 
+    return (
+      <div
+        ref={ref}
+        className="border rounded-md p-4 bg-white w-full max-w-md leading-tight"
+        style={{
+          fontSize: baseTextSize,
+        }}
+      >
+        <div className="prose">
+          <table
+            cellPadding="0"
+            cellSpacing="0"
+            style={{ fontFamily: "Arial, sans-serif" }}
+          >
+            <tbody>
+              <tr>
+                <td
+                  style={{
+                    verticalAlign: "top",
+                    paddingRight: "15px",
+                    width: data.logo ? `${logoWidth}px` : "0",
+                    minWidth: data.logo ? `${logoWidth}px` : "0",
+                  }}
+                >
+                  {data.logo && (
+                    <img
+                      src={data.logo}
+                      alt="Company logo"
+                      style={{
+                        width: "100%",
+                        height: "auto",
+                      }}
+                    />
+                  )}
+                </td>
+                <td style={{ verticalAlign: "top", color: baseTextColor }}>
+                  {!hasTextData && (
+                    <em className="opacity-50">
+                      Edit the fields to see a preview
+                    </em>
+                  )}
 
+                  {data.primaryLine && (
+                    <div
+                      style={{
+                        fontSize: "1em",
+                        fontWeight: "bold",
+                        color: primaryLineColor,
+                        marginBottom: "4px",
+                      }}
+                    >
+                      {data.primaryLine}
+                    </div>
+                  )}
+                  {data.optional && (
+                    <div style={{ fontSize: "0.92em", marginBottom: "4px" }}>
+                      {data.optional}
+                    </div>
+                  )}
+                  {data.address && (
+                    <div
+                      style={{
+                        fontSize: "0.92em",
+                        marginBottom: "4px",
+                        whiteSpace: "pre-line",
+                      }}
+                    >
+                      {data.address}
+                    </div>
+                  )}
 
-  return (
-    <div ref={ref} className="border rounded-md p-4 bg-white w-full max-w-md">
-      <div className="prose">
-        <table cellPadding="0" cellSpacing="0" style={{ fontFamily: 'Arial, sans-serif' }}>
-          <tbody>
-            <tr>
-              <td style={{
-                verticalAlign: 'top',
-                paddingRight: '15px',
-                width: data.logo ? `${logoWidth}px` : '0',
-                minWidth: data.logo ? `${logoWidth}px` : '0'
-              }}>
-                {data.logo && (
-                  <img
-                    src={data.logo}
-                    alt="Company logo"
-                    style={{
-                      width: '100%',
-                      height: 'auto'
-                    }}
-                  />
-                )}
-              </td>
-              <td style={{ verticalAlign: 'top', color: baseTextColor }}>
-                {!hasTextData && (
-                  <em className='opacity-50'>
-                    Edit the fields to see a preview
-                  </em>
-                )}
+                  {(data.phone || data.email) && (
+                    <>
+                      {" "}
+                      <hr className="my-6" />
 
-                {data.primaryLine && (
-                  <div style={{ fontSize: '16px', fontWeight: 'bold', color: '#2563eb', marginBottom: '4px' }}>
-                    {data.primaryLine}
-                  </div>
-                )}
-                {data.optional && (
-                  <div style={{ fontSize: '14px', color: '#4b5563', marginBottom: '4px' }}>
-                    {data.optional}
-                  </div>
-                )}
-                {data.address && (
-                  <div style={{ fontSize: '12px',  marginBottom: '4px', whiteSpace: 'pre-line' }}>
-                    {data.address}
-                  </div>
-                )}
-                {data.phone && (
-                  <div style={{ fontSize: '12px',  marginBottom: '2px' }}>
-                    Phone: {data.phone}
-                  </div>
-                )}
-                {data.email && (
-                  <div style={{ fontSize: '12px' }}>
-                    Email: <a href={`mailto:${data.email}`} style={{ color: emailTextColor, textDecoration: 'none' }}>{data.email}</a>
-                  </div>
-                )}
-              </td>
-            </tr>
-          </tbody>
-        </table>
+                      <div className="flex flex-col gap-2">
+                      {data.phone && (
+                        <div
+                          style={{ fontSize: "0.92em", marginBottom: "2px" }}
+                        >
+                          <span
+                            style={{ fontSize: "0.8em", width: "44px" }}
+                            className="inline-block relative opacity-75"
+                          >
+                            TEL:
+                          </span>
+                          {data.phone}
+                        </div>
+                      )}
+                      {data.email && (
+                        <div style={{ fontSize: "0.92em" }}>
+                          <span
+                            style={{ fontSize: "0.8em", width: "44px" }}
+                            className="inline-block relative opacity-75"
+                          >
+                            MAIL:
+                          </span>
+                          <a
+                            href={`mailto:${data.email}`}
+                            style={{
+                              color: emailTextColor,
+                              textDecoration: "none",
+                            }}
+                          >
+                            {data.email}
+                          </a>
+                        </div>
+                      )}
+                      </div>
+                    </>
+                  )}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
-  );
-});
+    );
+  },
+);
 
-SignaturePreview.displayName = 'SignaturePreview';
+SignaturePreview.displayName = "SignaturePreview";
 
 export default SignaturePreview;
